38ba6dda16873d0378fc25c68c32727feb831206
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2024-08-17 17:18:14 +0530

    conditionals

---
+++ 2. Conditionals/Age.py
@@ -0,0 +1,9 @@
+age = input("provide some age")
+if age<13:
+    print("you are a kid")
+elif age<20:
+    print("you are a teenageer")
+elif age < 60:
+    print("you are a senior")
+else:
+    print("you are old")
